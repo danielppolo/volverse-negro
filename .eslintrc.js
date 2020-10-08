1568392f15ddbd32b346b145bee92a749d543548
Daniel Polo <od.polo@gmail.com>
2020-10-07 23:50:07 -0500

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+module.exports = {
+  extends: [
+    'airbnb',
+    "plugin:react-hooks/recommended",
+  ],
+  parser: 'babel-eslint',
+  globals: {
+    document: true,
+    window: true,
+    oneSignal: true,
+    cy: true,
+    Cypress: true,
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
+  plugins: [
+    'react-hooks',
+  ],
+  rules: {
+    'react/no-danger': 0,
+    "react/prop-types": [2, { ignore: ['children'] }],
+    'import/no-named-as-default-member': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    "react-hooks/exhaustive-deps": 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 0,
+    semi: [2, 'never'],
+    quotes: [2, 'single'],
+    'jsx-quotes': [2, 'prefer-double'],
+    'no-restricted-imports': [2, { patterns: ['../*'] }],
+    'jsx-a11y/no-autofocus': [0, {
+      ignoreNonDOM: true,
+    }],
+    'no-underscore-dangle': ['error', {
+      allow: ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'],
+    }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname, 'src')],
+      },
+    },
+  },
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,25 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import useFigures from "../hooks/use-figures"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Volverse negro" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const figureList = useFigures()
+  console.log(figureList)
+  return (
+    <Layout>
+      <SEO title="Volverse negro" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+        {
+          figureList.map(f => f)
+        }
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
